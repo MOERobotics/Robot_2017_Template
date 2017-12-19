f8597f4ef9d26fc68b184fa0b1aa8ec5c0ee312c
Audrey <ayang24.robin@gmail.com>
2017-12-18 19:44:10 -0500

    changed ballflap

--- src/org/usfirst/frc/team365/routines/Teleop.java
+++ src/org/usfirst/frc/team365/routines/Teleop.java
@@ -115,7 +115,7 @@ public class Teleop extends RobotRoutine{
 			lastButtonD3 = false;
 		}
 		
-		if (inputs.funStick.getY()<-0.15) //changed from -0.5{
+		if (inputs.funStick.getY()<-0.15) { 
 			modules.ballflap.up();
 		} else {
 			modules.ballflap.down();
